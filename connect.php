a11f59c51cd123f005c5cb2c63c83dc2c80aa255
Jordan Taylor <jtaylor32@radford.edu>
2015-11-05 17:28:49 -0500

    added database connection file

---
+++ connect.php
@@ -0,0 +1,12 @@
+<?php
+// This is used to connect to database. Required anytime we wish to use database.
+$dbhost = 'localhost';
+$dbuser = 'jtaylor32';
+$dbpass = '123456';
+$dbname = 'jtaylor32';
+require_once( 'DB.php' );
+
+$db = DB::connect( "mysql://$dbuser:$dbpass@$dbhost/$dbname" );
+
+$db->setFetchMode(DB_FETCHMODE_ASSOC);
+?>
