fa023aca5cedc6a747ca757a9245eca9b5f6a83f
Jordan Taylor <jtaylor32@radford.edu>
2015-11-05 13:07:07 -0500

    updating changes for register page and links

--- register.php
+++ register.php
@@ -1,7 +1,3 @@
-<php?
-	session_start();
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -13,7 +9,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>About - SMIPO</title>
+    <title>Register - SMIPO</title>
 
     <!-- Bootstrap Core CSS -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
@@ -33,21 +29,115 @@
     <![endif]-->
 
 </head>
+
 <body>
-<form name="register" id="register" action="registerhandle.php">
-	<table>
-		<th>
-			Member Registration
-		</th>
-		<tr>
-			<td>
-				First Name:
-			</td>
-			<td>
-				
-			</td>
-		</tr>
-	</table>
-</form>
+
+    <div class="brand">Radford SMIPO</div>
+    <div class="address-bar">Radford's Division of SMIPO | <a href="https://www.radford.edu/content/radfordcore/home.html"> Radford University </a> | Radford, Virginia</div>
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-default" role="navigation">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <!-- navbar-brand is hidden on larger screens, but visible when the menu is collapsed -->
+                <a class="navbar-brand" href="index.html">SMIPO</a>
+            </div>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                    <li>
+                        <a href="index.html">Home</a>
+                    </li>
+                    <li>
+                        <a href="about.html">About</a>
+                    </li>
+                    <li>
+                        <a href="blog.html">Forum</a>
+                    </li>
+                    <li>
+                        <a href="contact.html">Register</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
+
+    <div class="container">
+    	<div class="row">
+            <div class="box">
+                <div class="col-lg-12">
+                    <hr>
+                    <h2 class="intro-text text-center">Enter your information
+                        <strong></strong>
+                    </h2>
+                    </hr>
+                    <p>
+                    	<form name="register" id="register" action="registerhandle.php" method="post">
+                    		<center>
+                    			<table>
+                    				<tr>
+                    					<td>First Name:</td>
+                    					<td><input type="text" width="25" name="firstname" id="firstname" onblur="validateUsername();" /></td>
+                    					<td id="firstname-err"></td>
+                    				</tr>
+                    				<tr>
+                    					<td>Last Name:</td>
+                    					<td><input type="text" width="25" name="lastname" id="lastname" onblur="validateUsername();" /></td>
+                    					<td id="lastname-err"></td>
+                    				</tr>
+                    				<tr>
+                    					<td>Desired Username:</td>
+                    					<td><input type="text" width="25" name="username" id="username" onblur="validateUsername();" /></td>
+                    					<td id="username-err"></td>
+                    				</tr>
+                    				<tr>
+                    					<td>Password</td>
+                    					<td><input type="password" width="25" name="password" id="password" onblur="validatePassword();" /></td>
+                    					<td id="password-err"></td>
+                    				</tr>
+                    				<tr>
+                    					<td>Verify Password</td>
+                    					<td><input type="password" width="25" name="ver_password" id="ver_password" onblur="validateVer_Password();" /></td>
+                    					<td id="ver_password-err"></td>
+                    				</tr>
+                    			</table>
+                    			<br />
+                    			<button name="register" type="submit" formmethod="post">Register</button>
+                    		</center>
+                    	</form>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- /.container -->
+
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <p>Copyright &copy; Radford SMIPO 2015</p>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
 </body>
+
 </html>
+<?php  ?>
