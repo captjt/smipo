90c1130dd1140dd57dd3663fc270ba3d3b9167b3
cwolf1038 <cwolf123456@yahoo.com>
2016-02-25 10:49:26 -0500

    Hopefully fix error in last push causing page to not render

--- boards.php
+++ boards.php
@@ -7,7 +7,7 @@
 <?php
 require("connect.php");
 $board_id = $_GET['id'];
-if(!IS_SET($_GET['startingThread'])){
+if(!is_set($_GET['startingThread'])){
 	$startingThread = 0;
 }else{
 	$startingThread = $_GET['startingThread'];
