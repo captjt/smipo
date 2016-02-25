6bdda692c8556527b1529fe7625177d729d96fc1
cwolf1038 <cwolf123456@yahoo.com>
2016-02-25 11:43:46 -0500

    Update boards.php

--- boards.php
+++ boards.php
@@ -7,7 +7,7 @@
 <?php
 require("connect.php");
 $board_id = $_GET['id'];
-if(!is_set($_GET['startingThread'])){
+if(!isset($_GET['startingThread'])){
 	$startingThread = 0;
 }else{
 	$startingThread = $_GET['startingThread'];
