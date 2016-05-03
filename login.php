a8da8183db7b18a7cb6ce2d08ca0c77387e0bef6
Jordan Taylor <jtaylor32@radford.edu>
2016-05-03 12:41:05 -0400

    Changed login

--- login.php
+++ login.php
@@ -65,7 +65,7 @@
     	<div class="container">
     		<div class="row">
     			<div class="col-lg-12 text-center">
-    				<p>Copyright &copy; Radford SMIPO 2015</p>
+    				<p>Copyright &copy; Radford SMIPO 2016</p>
     			</div>
     		</div>
     	</div>
