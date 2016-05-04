4e70e20b333056f301f086b3bcad90e166ae5e57
Jordan Taylor <jtaylor007.jt@gmail.com>
2016-05-03 21:59:18 -0400

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,9 +1,9 @@
 <?php
 	// This is used to connect to database. Required anytime we wish to use database.
 	$dbhost = 'localhost';
-	$dbuser = 'smipo';
-	$dbpass = 'Radford2016';
-	$dbname = 'smipo';
+	$dbuser = '***';
+	$dbpass = '***';
+	$dbname = '***';
 	require_once( 'DB.php' );
 
 	$db = DB::connect( "mysql://$dbuser:$dbpass@$dbhost/$dbname" );
